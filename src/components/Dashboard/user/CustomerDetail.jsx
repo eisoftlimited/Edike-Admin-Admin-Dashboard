ade8ff05ef082bf3f3268bf67a62ddc56a0e430b
devaladey <oluwatobioyedokun7@gmail.com>
2023-02-12 14:56:08 +0100

    ...

--- src/components/Dashboard/user/CustomerDetail.jsx
+++ src/components/Dashboard/user/CustomerDetail.jsx
@@ -263,19 +263,19 @@ function CustomerDetail() {
                         <div className={classes.images}>
                             <div>
                                 <h4>Utility bill</h4>
-                                <a href={beneficiary_file_results && beneficiary_file_results[0]?.secure_url} download='utility-bill'>
-                                <img src={customer && customer.houseAddressLink && customer.houseAddressLink[0]?.secure_url} alt='' />
+                                <a href={customer && customer.houseAddressLink && customer.houseAddressLink[0] && customer.houseAddressLink[0]?.secure_url} download='utility-bill'>
+                                <img src={customer && customer.houseAddressLink && customer.houseAddressLink[0] && customer.houseAddressLink[0]?.secure_url} alt='' />
                                 </a>
                             </div>
                             <div>
                                 <h4>Id Card</h4>
-                                <a href={beneficiary_file_results && beneficiary_file_results[0]?.secure_url} download='id-card'>
-                                <img src={customer && customer.idcard && customer.idcard[0]?.secure_url} alt='' />
+                                <a href={customer && customer.idcard && customer.idcard[0] && customer.idcard[0]?.secure_url} download='id-card'>
+                                <img src={customer && customer.idcard && customer.idcard[0] && customer.idcard[0]?.secure_url} alt='' />
                                 </a>
                             </div>
                             <div>
                                 <h4>Debit Card</h4>
-                                <a href={beneficiary_file_results && beneficiary_file_results[0]?.secure_url} download='utility-bill'>
+                                <a href={customer && customer.idcard && customer.idcard[0]?.secure_url} download='utility-bill'>
                                 <img src={customer && customer.idcard && customer.idcard[0]?.secure_url} alt='' />
                                 </a>
                                 
