c7a89f51ffce2ffb84247bd0e59b0681567ca2b5
devaladey <oluwatobioyedokun7@gmail.com>
2023-02-17 09:39:45 +0100

    ...

--- src/screens/Account/VerifyMobileScreen.jsx
+++ src/screens/Account/VerifyMobileScreen.jsx
@@ -101,7 +101,7 @@ function VerifyMobileScreen() {
         const email = localStorage.getItem('edike-admin-email');
 
         const data = { otpToken: otp.join(''), email };
-        // console.log(data);
+        console.log(data);
         dispatch(activateUser(data));
         setOtp(undefArray);
     };
