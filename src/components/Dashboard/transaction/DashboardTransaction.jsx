aecbafea196c267ac8cb4d38893d5b134acdce7e
devaladey <oluwatobioyedokun7@gmail.com>
2023-03-15 11:44:23 +0100

    Another commit

--- src/components/Dashboard/transaction/DashboardTransaction.jsx
+++ src/components/Dashboard/transaction/DashboardTransaction.jsx
@@ -3,7 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useOutletContext } from 'react-router-dom';
 import { getTransactionsRequest } from '../../../store/transaction/getTransactionsSlice';
 import { formatCurr } from '../../../utils/currencyFormater';
+import { exportFileToExcel } from '../../../utils/exportToExcel';
+import { ExportBtn } from '../DashBoardButtons';
 import DashBoardNav from '../DashBoardNav';
+import LoanCards from '../loan/loancards/LoanCards';
 import classes from './DashTransaction.module.scss';
 
 function DashboardTransaction() {
@@ -43,17 +46,20 @@ function DashboardTransaction() {
                 btnText='Add User'
                 showSearchNav={false}
             />
-
+            <LoanCards />
+            <div style={{ marginBottom: '1rem' }}>
+                <ExportBtn onExportTable={() => exportFileToExcel(transactions, 'Transactions')} />
+            </div>
             <div className={classes.tabcontainer}>
                 <table>
                     <thead>
                         <tr>
-                            <th>Transaction Id</th>
                             <th>Date</th>
-                            <th>Loan Id</th>
-                            <th>Customer Id</th>
                             <th>Customer Name</th>
                             <th>Customer Email</th>
+                            <th style={{textAlign: 'center'}}>Customer Id</th>
+                            <th style={{textAlign: 'center'}}>Loan Id</th>
+                            <th>Tranx Id</th>
                             <th>Amount</th>
                             <th>Channel</th>
                         </tr>
@@ -61,12 +67,12 @@ function DashboardTransaction() {
                     <tbody>
                         {transactions && transactions.map(transaction => (
                             <tr key={transaction._id}>
-                                <td className={classes['table-data']} title={transaction.reference}>{transaction.reference?.slice(0, 10)}...</td>
                                 <td className={classes['table-data']}>{transaction.createdAt && formatDate(transaction.createdAt)}</td>
-                                <td className={classes['table-data']}>{transaction.loan_ref}</td>
-                                <td className={classes['table-data']}>{transaction.cus_ref}</td>
                                 <td className={classes['table-data']}>{transaction.cus_name}</td>
                                 <td className={classes['table-data']} title={transaction.cus_email}>{transaction.cus_email?.slice(0, 15)}</td>
+                                <td style={{textAlign: 'center'}} className={classes['table-data']}>{transaction.cus_ref}</td>
+                                <td style={{textAlign: 'center'}} className={classes['table-data']}>{transaction.loan_ref}</td>
+                                <td className={classes['table-data']} title={transaction.reference}>{transaction.reference?.slice(0, 10)}...</td>
                                 <td className={classes['table-data']}>{transaction.amount && formatCurr(transaction.amount)}</td>
                                 <td className={classes['table-data']}>Card</td>
                             </tr>
